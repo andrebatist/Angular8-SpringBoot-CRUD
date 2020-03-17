ad1ac7168c2999ef688d08ca7d38ae9966b689fe
aplaksin <aplaksin@ecocompany.ru>
2020-03-17 15:02:12 +0300

    init

---
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2jpacrudexample/model/Employee.java
@@ -0,0 +1,26 @@
+package net.guides.springboot2jpacrudexample.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email_address", nullable = false)
+    private String emailId;
+}
